2d139d20a10c8b169bbfaf190bcfc8206145ee62
JeffreyMojica <josuejeff17@gmail.com>
2022-12-17 01:02:06 -0600

    eliminacionCliente

--- PROYECTO-FINAL-EDD/src/clsGestionCliente.java
+++ PROYECTO-FINAL-EDD/src/clsGestionCliente.java
@@ -26,11 +26,9 @@ public class clsGestionCliente {
         return this.largo;
     }
 
-    //SIRVE PARA LA INTERFAZ
     public void registrarCliente(clsCliente c) {
 
         clsNodoCliente newNode = new clsNodoCliente(c);
-        // newNode.setDato(c);
 
         if (this.esVacia()) {
             this.cima = newNode;
@@ -39,58 +37,8 @@ public class clsGestionCliente {
             this.cima = newNode;
         }
         this.largo++;
-        /* try {
-            JOptionPane.showMessageDialog(null, "¡Datos agregados correctamente!",
-                    "Datos agregados", JOptionPane.INFORMATION_MESSAGE);
-        } catch (Exception ex01) {
-            JOptionPane.showMessageDialog(null, "¡Error al guardar, revise!", "Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }*/
-
     }
 
-    /*public void AvanzarLista(String id) {
-        clsNodoCliente aux = this.cima;
-        clsNodoAlquiler auxA = cabeza;
-        clsAtributosAlquiler alquiler = new clsAtributosAlquiler();
-       // int cedula = Integer.parseInt(aux.getDato().getCedula());
-        //String id;
-         
-          
-
-        
-        id = alquiler.getId();
-            while (auxA.getNext() != null && auxA.getDato().getId() != id) {
-            System.out.println("Hola1");
-
-            auxA = auxA.getNext();
-            System.out.println("aaaaa");
-        }
-
- while (clsNodoAlquiler.next != null && clsNodoAlquiler.dato.getId() != id) {
-            System.out.println("Hola1");
-
-            auxA = auxA.getNext();
-            System.out.println("aaaaa");
-        }
-
-        while (aux.getDato().getCedula() != id) {
-            System.out.println("Hola2C");
-            aux = aux.getNext();
-
-        }
-
-        if (auxA.getDato().getCantDias() == Integer.parseInt(aux.getDato().getCedula())) {
-            System.out.println("Hola3");
-            if (auxA.getDato().getCantDias() >= 30) {
-                System.out.println("Hola4");
-                //varios ifs para categoria
-                aux.getDato().setCategoria("ORO");
-            }
-        }
-
-    }*/
-    //SIRVE PARA LA INTERFAZ
     public String buscarCliente(String cedula) {
         clsNodoCliente aux = this.cima;
         String cedulaCliente = " ", nombre = " ", nacimiento = " ", correo = " ", categorias = " ";
@@ -116,7 +64,6 @@ public class clsGestionCliente {
         return cedulaCliente + "," + nombre + "," + nacimiento + "," + correo + "," + categorias;
     }
 
-//REVISAR
     public void InfoAlquileres(String cedula) {
         clsNodoCliente aux = this.cima;
         clsNodoAlquiler aux2 = cabeza;
@@ -142,7 +89,6 @@ public class clsGestionCliente {
         }
     }
 
-    //SIRVE PARA LA INTERFAZ
     public boolean editarCliente(String cedula, String nombre, String nacimiento, String correo, String categorias) {
         clsNodoCliente aux = this.cima;
         int contador = 1;
@@ -170,7 +116,6 @@ public class clsGestionCliente {
         return respuesta;
     }
 
-//SIRVE PARA EL POP MODIFICADO(ELIMINAR CLIENTE OSEA SEARCH)
     public void pop() {
         if (!esVacia()) {
             // Asigna como primer node al siguiente de la pila.
@@ -180,7 +125,6 @@ public class clsGestionCliente {
         }
     }
 
-    //SIRVE PARA LA INTERFAZ
     public boolean search(String cedula) {
         // Crea una copia de la pila.
         clsNodoCliente aux = cima;
@@ -188,91 +132,72 @@ public class clsGestionCliente {
         boolean exist = false;
 
         //if (!clsNodoAlquiler.dato.getId().equalsIgnoreCase(cedula)) {
-            // Recorre la pila hasta llegar encontrar el node o llegar al final
-            // de la pila.
-            while (exist != true && aux != null) {
-                // Compara si el value del node es igual que al de reference.
-                if (cedula == aux.getDato().getCedula()) {
-                    // Cambia el value de la bandera.
-                    exist = true;
-                    if (exist = true) {
-                        clsNodoCliente cimapPilaAux = null;
-                        // Recoore la pila hasta llegar al node que tenga el value
-                        // igual que el de reference.
-                        //  clsNodoCliente aux = cima;
-                        while (cedula != cima.getDato().getCedula()) {
-                            // Crea un node temporal para agregarlos a la pila auxiliar.
-                            clsNodoCliente temp = new clsNodoCliente();
-                            // Ingresa el value al node temporal.
+        // Recorre la pila hasta llegar encontrar el node o llegar al final
+        // de la pila.
+        while (exist != true && aux != null) {
+            // Compara si el value del node es igual que al de reference.
+            if (cedula == aux.getDato().getCedula()) {
+                // Cambia el value de la bandera.
+                exist = true;
+                if (exist = true) {
+                    clsNodoCliente cimapPilaAux = null;
+                    // Recoore la pila hasta llegar al node que tenga el value
+                    // igual que el de reference.
+                    //  clsNodoCliente aux = cima;
+                    while (cedula != cima.getDato().getCedula()) {
+                        // Crea un node temporal para agregarlos a la pila auxiliar.
+                        clsNodoCliente temp = new clsNodoCliente();
+                        // Ingresa el value al node temporal.
 
-                            //---temp.setValor(cima.getValor());
-                            //--- temp.setDato(cima.getDato().getCedula());
-                            temp.setDato(cima.getDato());
+                        //---temp.setValor(cima.getValor());
+                        //--- temp.setDato(cima.getDato().getCedula());
+                        temp.setDato(cima.getDato());
 
-                            // Consulta si la pila auxiliar no a sido inicializada.
-                            if (cimapPilaAux == null) {
-                                // Inicializa la pila auxiliar.
-                                cimapPilaAux = temp;
-                            } // Caso contrario si la pila auxiliar ya contiene elementos
-                            // los agrega al start.
-                            else {
-                                temp.setNext(cimapPilaAux);
-                                cimapPilaAux = temp;
-                            }
-                            // Elimina el node del tope de la pila hasta llegar al node
-                            // que se desea eliminar.
-                            pop();
-                            this.largo--;
+                        // Consulta si la pila auxiliar no a sido inicializada.
+                        if (cimapPilaAux == null) {
+                            // Inicializa la pila auxiliar.
+                            cimapPilaAux = temp;
+                        } // Caso contrario si la pila auxiliar ya contiene elementos
+                        // los agrega al start.
+                        else {
+                            temp.setNext(cimapPilaAux);
+                            cimapPilaAux = temp;
                         }
-                        // Elimina el node que coincide con el de reference.
+                        // Elimina el node del tope de la pila hasta llegar al node
+                        // que se desea eliminar.
                         pop();
                         this.largo--;
-                        // Regresa los valuees de la pila auxiliar a la pila original
-                        // mientras la pila auxiliar tenga elementos.
-                        while (cimapPilaAux != null) {
-                            // Utiliza el metodo push para regresar los elementos a 
-                            // la pila original.
-
-                            registrarCliente(cimapPilaAux.getDato());
-                            //--- push(cimapPilaAux.setDato());
-
-                            // Avanza al siguiente node de la pila auxiliar.
-                            cimapPilaAux = cimapPilaAux.getNext();
-                        }
-                        // Libera la memoria utilizada por la pila auxiliar.
-                        cimapPilaAux = null;
-                    } else {
-                        System.out.println("El nodo indicado no existe");
                     }
+                    // Elimina el node que coincide con el de reference.
+                    pop();
+                    this.largo--;
+                    // Regresa los valuees de la pila auxiliar a la pila original
+                    // mientras la pila auxiliar tenga elementos.
+                    while (cimapPilaAux != null) {
+                        // Utiliza el metodo push para regresar los elementos a 
+                        // la pila original.
+
+                        registrarCliente(cimapPilaAux.getDato());
+                        //--- push(cimapPilaAux.setDato());
+
+                        // Avanza al siguiente node de la pila auxiliar.
+                        cimapPilaAux = cimapPilaAux.getNext();
+                    }
+                    // Libera la memoria utilizada por la pila auxiliar.
+                    cimapPilaAux = null;
                 } else {
-                    // Avanza al siguiente node.
-                    aux = aux.getNext();
+                    System.out.println("El nodo indicado no existe");
                 }
+            } else {
+                // Avanza al siguiente node.
+                aux = aux.getNext();
             }
-        //} /*else {
-            //JOptionPane.showMessageDialog(null, "El Cliente tiene un Alquiler Registrado");
-        //}*/
+        }
 
         // Retorna el value de la bandera.
         return exist;
     }
 
-    /*  public void removerUltimo() {
-        if (this.esVacia()) {
-            JOptionPane.showMessageDialog(null, "La lista está vacia");
-        } else {
-             clsNodoCliente aux = this.cima;
-            //si hay un solo nodo
-            if (this.cima.getNext()== null) {
-                cima = null;
-            } else {
-                this.ultimo.getAnterior().setSiguiente(null);
-            }
-            this.ultimo = ultimo.getAnterior();
-             this.largo--;
-        }
-    }*/
-    //No sirve
     public void eliminarCliente(String cedula) {
         // Consulta si el value exist en la pila.
         if (search2(cedula)) {
@@ -349,74 +274,6 @@ public class clsGestionCliente {
         return exist;
     }
 
-    //No sirve
-    public boolean modificarCliente(clsCliente c) {
-        // Crea una copia de la pila.
-        clsNodoCliente aux = cima;
-
-        // Bandera para verificar si exist el elemento a search.
-        boolean exist = false;
-        // Recorre la pila hasta llegar encontrar el node o llegar al final
-        // de la pila.
-        while (exist != true && aux != null) {
-            // Compara si el value del node es igual que al de reference.
-            if (aux.getDato().getCedula() == c.getCedula()) {
-                aux.getDato().setNombre(c.getNombre());
-                aux.getDato().setNacimiento(c.getNacimiento());
-                aux.getDato().setCorreo(c.getCorreo());
-                aux.getDato().setCategoria(c.getCategoria());
-                exist = true;
-            } else {
-                // Avanza al siguiente node.
-                aux = aux.getNext();
-            }
-        }
-        // Retorna el value de la bandera.
-        return exist;
-    }
-
-    /* public boolean esPosicionValida(int indice) {
-        return (indice > 0 && indice <= this.largo) || (indice > 0 && indice == this.largo + 1);
-    }*/
-
- /* public boolean removerEnPosicion(int posicion) {
-        boolean bandera = false;
-        if (!this.esPosicionValida(posicion)) {
-            bandera = false;
-        } else {
-            bandera = true;
-            clsNodoCliente aux = this.cima;
-            if (posicion == 1) {
-                this.pop();
-            } else if (posicion == this.largo) {
-                this.eliminarCliente(posiscion);
-            } else {
-                for (int i = 1; i < posicion && aux.getNext() != null; i++) {
-                    aux = aux.getNext();
-                }
-                //actual.getAnterior().setSiguiente(actual.getSiguiente());
-                aux.getNext().setNext(aux.getNext());
-                this.largo--;
-            }
-        }
-        return bandera;
-    }*/
-
- /* public void removerPrimero() {
-        if (this.esVacia()) {
-            JOptionPane.showMessageDialog(null, "La lista está vacia");
-        } else {
-            //si hay un solo nodo
-            if (this.cima.getNext() == null) {
-            } else {
-                cima.getNext().setAnterior(null);
-            }
-            this.primero = primero.getSiguiente();
-            this.cantNodos--;
-        }
-    }*/
-    //SIRVE PARA CONSULTAR Y MOSTRAR LOS DATOS
-//SIRVE PARA LA INTERFAZ NO SE PARA QUE
     public String ConsultarCliente(String cedula) {
 
         // Crea una copia de la pila.
@@ -453,30 +310,6 @@ public class clsGestionCliente {
         return cedula + "," + nombre + "," + nacimiento + "," + correo + "," + categorias;
     }
 
-    //SIRVE PERO SOLO MUESTRA LA CEDULA
-    public void Consulta(String cedula) {
-        if (cima != null) {
-
-            clsCliente c = new clsCliente();
-            //   clsNodoCliente aux = cima;
-
-            clsNodoCliente aux = new clsNodoCliente(c);
-
-            // clsNodoCliente aux = new clsNodoCliente();
-            //  aux.setDato(c);
-            // Recorre la pila hasta llegar encontrar el node o llegar al final
-            // de la pila.
-            while (aux != null && cedula == aux.getDato().getCedula()) {
-                aux = aux.getNext();
-            }
-
-            System.out.println(aux);
-
-        }
-
-    }
-
-    //SIRVE
     public void desplegarlista() {
         // Crea una copia de la pila.
         clsNodoCliente aux = new clsNodoCliente();
@@ -494,37 +327,4 @@ public class clsGestionCliente {
 
     }
 
-
-    /*//NO SIRVE 
-    public void listar() {
-        // Crea una copia de la pila.
-        clsNodoCliente aux = cima;
-        // Recorre la pila hasta el ultimo node.
-        while (aux != null) {
-            System.out.println("|\t" + aux.getDato().getCedula() + " " + aux.getDato().getNombre()
-                    + aux.getDato().getNacimiento() + " " + aux.getDato().getCorreo() + aux.getDato().getCategoria() + "\t|");
-            System.out.println("-----------------");
-            aux = aux.getNext();
-        }
-    }
-     
-
-       @Override
-    public String toString() {
-        clsNodoCliente aux = cima;
-        String s = "Lista: ";
-        if (aux != null) {
-            s += aux + ", ";
-           aux = aux.getNext();
-            while (aux != cima) {
-                s += aux + ", ";
-               aux = aux.getNext();
-            }
-        } else {
-            s += "vacia";
-        }
-        return s;
-    }
-     
-     */
 }
